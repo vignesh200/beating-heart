4eb51baaa2f84fda4dc48aebca28e8cdcda8e96b
vignesh200 <76198120+vignesh200@users.noreply.github.com>
2021-01-30 15:32:49 +0530

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,4 @@
+def on_forever():
+    basic.show_string("Hello! wellcome to MICROBIT")
+    basic.show_icon(IconNames.HAPPY)
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Hello! wellcome to MICROBIT")
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
